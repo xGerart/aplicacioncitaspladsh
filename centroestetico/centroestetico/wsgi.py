6f6cda016e9946191e2f6b0b2c75bea3d7691a80
xGerart <gerart674@gmail.com>
2024-08-16 00:56:19 -0500

    Update wsgi.py

--- centroestetico/centroestetico/wsgi.py
+++ centroestetico/centroestetico/wsgi.py
@@ -7,22 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-#import os
-
-#from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'centroestetico.settings')
-
-#application = get_wsgi_application()
-
 import os
-import sys
-
-path = '/home/gerart674/aplicacioncitaspladsh'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'centroestetico.settings'
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'centroestetico.settings')
+
 application = get_wsgi_application()
+
