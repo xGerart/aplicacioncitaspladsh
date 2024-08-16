4c48469b5819ecc3ce4701df582467a3ad12b2c9
xGerart <gerart674@gmail.com>
2024-08-15 19:42:17 -0500

    BDD

--- centroestetico/centroestetico/settings.py
+++ centroestetico/centroestetico/settings.py
@@ -88,13 +88,17 @@ WSGI_APPLICATION = 'centroestetico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'gerart$aplicacioncitaspladsh',
+        'USER':'gerart674',
+        'PASSWORD':'172503Gt',
+        'HOST':'gerart674.mysql.pythonanywhere-services.com',
+        'PORT':'3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
