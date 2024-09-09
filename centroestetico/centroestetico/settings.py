459bf794b64a14df4d6f41e3c055b1dd87a759cc
xGerart <gerart674@gmail.com>
2024-09-09 14:29:23 -0500

    settings

--- centroestetico/centroestetico/settings.py
+++ centroestetico/centroestetico/settings.py
@@ -196,13 +196,13 @@ STATIC_ROOT = "/home/gerart674/aplicacioncitaspladsh/static"
 
 ## DESARROLLO LOCAL
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
-DATABASES = {
-                           "default": {
-                           "ENGINE": "django.db.backends.sqlite3",
-                           "NAME": BASE_DIR / "db.sqlite3",
-                        }
-                    }
+# DATABASES = {
+#                            "default": {
+#                            "ENGINE": "django.db.backends.sqlite3",
+#                            "NAME": BASE_DIR / "db.sqlite3",
+#                         }
+#                     }
 
 
